b992f5018ecafebc0f11c351d1b5e07bb3c147d3
Mayerson <mayerson18@gmail.com>
2017-07-19 20:06:48 -0400

    algunos arreglos

--- js/index.js
+++ js/index.js
@@ -11,7 +11,23 @@ $(".nav-toggle").click(function() {
 $('#owl-demo').owlCarousel({
   nav: true,
   items: 2,
-  margin: 56
+  margin: 56,
+  responsiveClass: true,
+  responsive:{
+       0:{
+           items:1,
+           nav:true
+       },
+       600:{
+           items:1,
+           nav:true
+       },
+       1000:{
+           items:2,
+           nav:false,
+           loop:false
+       }
+   }
 });
 
 $(".owl-prev").text("");
